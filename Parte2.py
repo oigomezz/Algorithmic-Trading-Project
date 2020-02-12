8780d83cb68cb8171ccd228f4e5099bc9022620a
Oscar <oigomezz@unal.edu.co>
2020-02-12 10:20:34 -0500

    Parte2

--- Parte2.py
+++ Parte2.py
@@ -10,12 +10,18 @@
 
 # Pipeline and Quantopian Trading Functions
 import quantopian.algorithm as algo
+from quantopian.algorithm import attach_pipeline, pipeline_output, order_optimal_portfolio
 import quantopian.optimize as opt
+from quantopian.optimize import TargetWeights
 from quantopian.pipeline import Pipeline, CustomFactor 
 from quantopian.pipeline.factors import Returns
 from quantopian.pipeline.data.builtin import USEquityPricing
 from quantopian.pipeline.filters import QTradableStocksUS
 from quantopian.pipeline.data import Fundamentals
+from quantopian.pipeline.factors import SimpleMovingAverage
+from quantopian.pipeline.data.psychsignal import stocktwits
+from quantopian.pipeline.data.sentdex import sentiment
+from quantopian.pipeline.data.psychsignal import twitter_withretweets as twitter_sentiment
 
 # The basics
 from collections import OrderedDict
@@ -25,17 +31,18 @@ import numpy as np
 
 # SKLearn :)
 from sklearn.naive_bayes import GaussianNB
+from sklearn import tree
 
 ##################################################
 # Globals
 ##################################################
 
 num_holding_days = 5 # holding our stocks for five trading days.
-days_for_fundamentals_analysis = 60
+days_for_fundamentals_analysis = 30
 upper_percentile = 90
 lower_percentile = 100 - upper_percentile
 MAX_GROSS_EXPOSURE = 1.0
-MAX_POSITION_CONCENTRATION = 0.01
+MAX_POSITION_CONCENTRATION = 0.05
 
 ##################################################
 # Initialize
@@ -63,7 +70,23 @@ def initialize(context):
 ##################################################
             
 class Predictor(CustomFactor):
-    """ Defines our machine learning model. """
+    
+    value = Fundamentals.ebit.latest / Fundamentals.enterprise_value.latest
+    quality = Fundamentals.roe.latest
+    sentiment_score = SimpleMovingAverage(
+        inputs=[stocktwits.bull_minus_bear],
+        window_length=3,
+    )
+    Fundamentals.ebit
+
+    universe = QTradableStocksUS()
+    value_winsorized = value.winsorize(min_percentile=0.05, max_percentile=0.95)
+    quality_winsorized = quality.winsorize(min_percentile=0.05, max_percentile=0.95)
+    sentiment_score_winsorized = sentiment_score.winsorize(min_percentile=0.05,max_percentile=0.95)
+    
+    mean_sentiment_5day = SimpleMovingAverage(inputs=[sentiment.sentiment_signal], window_length=5).winsorize(min_percentile=0.05, max_percentile=0.95)
+    positive_sentiment_pct = (twitter_sentiment.bull_scored_messages.latest/ twitter_sentiment.total_scanned_messages.latest).winsorize(min_percentile=0.05, max_percentile=0.95)
+    workingCapital = Fundamentals.working_capital_per_share.latest.winsorize(min_percentile=0.05, max_percentile=0.95)
     
     # The factors that we want to pass to the compute function. We use an ordered dict for clear labeling of our inputs.
     factor_dict = OrderedDict([
@@ -105,16 +128,12 @@ class Predictor(CustomFactor):
              y.iloc[index] = row
             
         y=y.stack(dropna=False)
-        
-        
-        ## Get rid of our y value as an input into our machine learning algorithm.
-        del inputs['Return']
 
         ## Munge x and y
         x = pd.concat([df.stack(dropna=False) for df in inputs.values()], axis=1).fillna(0)
         
         ## Run Model
-        model = GaussianNB() 
+        model = tree.DecisionTreeClassifier()
         model_x = x[:-num_secs*(num_holding_days+1)]
         model_y = y[:-num_secs*(num_holding_days+1)]
         model.fit(model_x, model_y)
